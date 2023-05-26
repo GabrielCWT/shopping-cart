3239467e1319a15bf0e2f972d95c4c6327857c50
CARR0T02 <mr.gabriel.chang@gmail.com>
2023-05-26 15:54:29 +0800

    Implement Cart

---
+++ src/pages/Cart.jsx
@@ -0,0 +1,14 @@
+import React, { useContext } from 'react';
+import { CartContext } from '../App';
+import CartCard from '../components/CartCard';
+
+export default function Cart() {
+  const cart = useContext(CartContext);
+  return (
+    <div className='cart'>
+      {cart.map((productObj) => (
+        <CartCard key={productObj.name} product={productObj} />
+      ))}
+    </div>
+  );
+}
