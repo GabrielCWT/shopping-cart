c061f46c123623904710aa0d1745cbbe2b420205
CARR0T02 <mr.gabriel.chang@gmail.com>
2023-06-13 10:07:00 +0800

    Reworked Cart animation

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,5 +1,5 @@
 import '../styles/Cart.scss';
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext } from 'react';
 import { CartContext } from '../App';
 import CartCard from '../components/CartCard';
 
